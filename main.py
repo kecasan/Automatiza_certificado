8a7016dd7575e1b09382e60929b61ab5b9efdcbf
Nardar-Cave <raphael.anderson2005@gmail.com>
2024-10-19 04:11:48 +0000

    Agora vai

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)):
     cnpj_numero = linha[2].value
     carga_horaria = linha[3].value
 
-    fonte_nome = ImageFont.truetype('./SUSE-Medium.ttf',90)
+    fonte_nome = ImageFont.truetype('./SUSE-Medium.ttf', 20)
 
     image = Image.open('./certificado.png')
     desenhar = ImageDraw.Draw(image)
 
-    desenhar.text((200,600), pessoa_nome, fill='black', font=fonte_nome)
-    desenhar.text((202,600), curso_nome, fill='royalblue', font=fonte_nome)
-    desenhar.text((204,600), cnpj_numero, fill='red', font=fonte_nome)
-    desenhar.text((206,600), carga_horaria, fill='green', font=fonte_nome)
+    desenhar.text((200,300), pessoa_nome, fill='black', font=fonte_nome)
+    desenhar.text((302,300), curso_nome, fill='black', font=fonte_nome)
+    desenhar.text((404,300), cnpj_numero, fill='black', font=fonte_nome)
+    desenhar.text((506,200), carga_horaria, fill='black', font=fonte_nome)
 
 
 
